f4eaf50aec8b8b5d882f82ac35af22c3720ce97c
YogGanz <86444100+YogGanz@users.noreply.github.com>
2021-08-09 21:12:25 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -1791,51 +1791,21 @@ router.get("/photooxy/smoke", async(req, res, next) => {
     	res.json(loghandler.invalidKey)
     }
 });
-router.get("/photooxy/burn-papper", async(req, res, next) => {
-        var theme = req.query.theme,
-             text = req.query.text,
-             apikeyInput = req.query.apikey;
-        
-	    if(!apikey) return res.json(loghandler.notparam)
-        if(listkey.includes(apikey)){
-        if (!theme) return res.json(loghandler.nottheme)
-        if (theme != 'text-burn' && theme != 'art-quote') return res.json(loghandler.notheme)
-        if (!text) return res.json(loghandler.nottext)
 
-        if (theme == 'text-burn') {
-            try {
-            request.post({
-                url: "https://photooxy.com/logo-and-text-effects/write-text-on-burn-paper-388.html",
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: `text_1=${text}&login=OK`,
-                }, (e,r,b) => {
-                    if (!e) {
-                        $ = cheerio.load(b)
-                        $(".thumbnail").find("img").each(function() {
-                            h = $(this).attr("src")
-                            var result = "https://photooxy.com/"+h
-                            fetch(encodeURI(`https://api.imgbb.com/1/upload?expiration=120&key=761ea2d5575581057a799d14e9c78e28&image=${result}&name=${randomTextNumber}`))
-                                .then(response => response.json())
-                                .then(data => {
-                                    var urlnya = data.data.url,
-                                        delete_url = data.data.delete_url;
-                                        res.json({
-                                            status : true,
-                                            creator : `${creator}`,
-                                            result:{
-                                                url:urlnya
-                                            }
-                                        })
-                                })
-                        })
-                    }
-                })
-                } catch (e) {
-                	console.log(e);
-                res.json(loghandler.error)
-                }
+router.get("/photooxy/burn-papper", async(req, res, next) => {
+  const text = req.query.text;
+  const apikey = req.query.apikey;
+  if(!text) return res.json(loghandler.nottext)
+  if(!apikey) return res.json(loghandler.notparam)
+  if(listkey.includes(apikey)) {
+  let hasil = 'https://yog-ganz.herokuapp.com/api/textmaker/random?text='+ text +'&theme=text-burn&apikey=YogGanz' 
+  data = await fetch(hasil).then(v => v.buffer())
+  await fs.writeFileSync(__path +'/tmp/burn-papper.jpg', data)
+  res.sendFile(__path +'/tmp/burn-papper.jpg')
+  } else {
+    res.json(loghandler.invalidKey)
+  }
+});
 
 router.get("/photooxy/naruto", async(req, res, next) => {
   const text1 = req.query.text;
